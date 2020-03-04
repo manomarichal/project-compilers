91ea89f88f06ff9d14857677e2af2e44c97345e7
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2020-03-04 17:17:28 +0100

    fixed accept() function side-effect: now moderately less janky, but more effort to maintain

--- src/AST.py
+++ src/AST.py
@@ -1,21 +1,16 @@
 # Implemented according to the composite design pattern
 # some of these classes are mostly just interfaces / use for type-checking if needed
 
+# genericAccept() not for external use!
+
 
 # Component
 class Component:
-    __parent: None
-    __name: None
+    __parent = None
+    __name = None
 
     def __init__(self, name=None):
         self.__name = name
-
-    def accept(self, visitor):
-        visitName = "visit" + type(self).__name__
-        if hasattr(visitor, visitName):
-            return getattr(visitor, visitName)(self)
-        else:
-            return visitor.visitChildren(self)
 
     def getName(self):
         return self.__name
@@ -25,6 +20,15 @@ class Component:
 
     def getParent(self):
         return self.__parent
+
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Component", lambda x: None)
+
+    def genericAccept(self, visitor, name, nextAttempt):
+        if hasattr(visitor, "visit" + name):
+            return getattr(visitor, "visit" + name)(self)
+        else:
+            return nextAttempt(visitor)
 
 
 # Composites
@@ -60,14 +64,17 @@ class Composite(Component):
         tmp = self.__children
         self.__children = other.__children
         other.__children = tmp
-    
+
         other.__children = tmp
-    
+
         for child in self.__children:
             child.__parent = self
-    
+
             for child in other.__children:
                 child.__parent = other
+
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Composite", super().accept)
 
 
 class DummyNode(Composite):
@@ -82,87 +89,116 @@ class DummyNode(Composite):
             assert isinstance(children, Component)
             self.addChild(children)
 
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "DummyNode", super().accept)
+
 
 class Doc(Composite):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Doc", super().accept)
 
 
 class Operator(Composite):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Operator", super().accept)
 
 
 class Neg(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Neg", super().accept)
 
 
 class Pos(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Pos", super().accept)
 
 
 class Prod(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Prod", super().accept)
 
 
 class Div(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Div", super().accept)
 
 
 class Mod(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Mod", super().accept)
 
 
 class Sum(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Sum", super().accept)
 
 
 class Sub(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Sub", super().accept)
 
 
 class Not(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Not", super().accept)
 
 
 class And(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "And", super().accept)
 
 
 class Or(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Or", super().accept)
 
 
 class Equal(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Equal", super().accept)
 
 
 class NotEqual(Operator):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "NotEqual", super().accept)
 
 
-class Less (Operator):
-    pass
+class Less(Operator):
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Less", super().accept)
 
 
-class LessE (Operator):
-    pass
+class LessE(Operator):
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "LessE", super().accept)
 
 
-class More (Operator):
-    pass
+class More(Operator):
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "More", super().accept)
 
 
-class MoreE (Operator):
-    pass
+class MoreE(Operator):
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "MoreE", super().accept)
+
 
 # Leaves
 class Leaf(Component):
-    pass
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Leaf", super().accept)
 
 
 class Literal(Leaf):
     val = None
 
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "Literal", super().accept)
+
 
 class IntLit(Literal):
     def __init__(self, val: int = 0):
         self.val = val
+
+    def accept(self, visitor):
+        return self.genericAccept(visitor, "IntLit", super().accept)
