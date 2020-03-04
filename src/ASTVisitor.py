bd0ef06eacd5cc19d784c9500959fd0ae6fc1112
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2020-03-04 14:37:17 +0100

    implementing visitor pattern for AST

--- src/ASTVisitor.py
+++ src/ASTVisitor.py
@@ -1,4 +1,33 @@
 from src import AST
 
+
 class ASTVisitor:
-    pass
+    def visit(self, tree):
+        return tree.accept(self)
+
+    def visitChildren(self, node):
+        result = self.defaultResult()
+        n = node.getChildCount()
+        for i in range(n):
+            if not self.shouldVisitNextChild(node, result):
+                return result
+
+            c = node.getChild(i)
+            childResult = c.accept(self)
+            result = self.aggregateResult(result, childResult)
+
+        return result
+
+    def defaultResult(self):
+        return None
+
+    def aggregateResult(self, aggregate, nextResult):
+        if nextResult is None:
+            return aggregate
+        if aggregate is None:
+            return [nextResult]
+        aggregate.append(nextResult)
+        return aggregate
+
+    def shouldVisitNextChild(self, node, currentResult):
+        return True
