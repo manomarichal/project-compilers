7004199c66180a1b113525064309b389bd8e92f6
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2020-03-04 15:40:28 +0100

    changed according to new directory structure

--- src/generate.py
+++ src/generate.py
@@ -1,3 +1,3 @@
 from os import system
 
-system("java -jar antlr-4.8-complete.jar -Dlanguage=Python2 -no-listener -visitor -o ./antlr Math.g4")
+system("java -jar antlr-4.8-complete.jar -Dlanguage=Python2 -no-listener -visitor -o ./antlr src/Math.g4")
