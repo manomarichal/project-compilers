2b597db8ac3e0bb83b4ae045bfc3bc1259cea317
sharogue <mano.marichal@student.uantwerpen.be>
2020-03-04 21:16:59 +0100

    Constant folding

--- src/ST/AST.py
+++ src/ST/AST.py
@@ -100,89 +100,111 @@ class Doc(Composite):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Doc", super().accept)
 
+
 class BinaryOp(Composite):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Operator", super().accept)
+
 
 class UnaryOp(Composite):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Operator", super().accept)
 
+
 class MathOp(BinaryOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Operator", super().accept)
+
 
 class LogicOp(BinaryOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Operator", super().accept)
 
+
 class CompOp(BinaryOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Operator", super().accept)
+
 
 class Neg(UnaryOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Neg", super().accept)
 
+
 class Pos(UnaryOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Pos", super().accept)
+
 
 class Prod(MathOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Prod", super().accept)
 
+
 class Div(MathOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Div", super().accept)
+
 
 class Mod(MathOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Mod", super().accept)
 
+
 class Sum(MathOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Sum", super().accept)
+
 
 class Sub(MathOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Sub", super().accept)
 
+
 class Not(LogicOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Not", super().accept)
+
 
 class And(LogicOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "And", super().accept)
 
+
 class Or(LogicOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Or", super().accept)
+
 
 class Equal(CompOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Equal", super().accept)
 
+
 class NotEqual(CompOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "NotEqual", super().accept)
+
 
 class Less(CompOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "Less", super().accept)
 
+
 class LessE(CompOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "LessE", super().accept)
+
 
 class More(CompOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "More", super().accept)
 
+
 class MoreE(CompOp):
     def accept(self, visitor):
         return self._genericAccept(visitor, "MoreE", super().accept)
+
 
 # Leaves
 class Leaf(Component):
--- src/ST/ASTCFVisitor.py
+++ src/ST/ASTCFVisitor.py
@@ -16,20 +16,58 @@ class ASTCFVisitor:
             if isinstance(ast.getChild(0), AST.IntLit) and isinstance(ast.getChild(1), AST.IntLit):
                 value = 0
 
-                if (isinstance(ast, AST.Sum)):
+                if isinstance(ast, AST.Sum):
                     value = ast.getChild(0).getValue() + ast.getChild(1).getValue()
-                elif (isinstance(ast, AST.Sub)):
+                elif isinstance(ast, AST.Sub):
                     value = ast.getChild(0).getValue() - ast.getChild(1).getValue()
-                elif (isinstance(ast, AST.Prod)):
+                elif isinstance(ast, AST.Prod):
                     value = ast.getChild(0).getValue() * ast.getChild(1).getValue()
-                elif (isinstance(ast, AST.Div)):
+                elif isinstance(ast, AST.Div):
                     value = ast.getChild(0).getValue() / ast.getChild(1).getValue()
-                elif (isinstance(ast, AST.Mod)):
+                elif isinstance(ast, AST.Mod):
                     value = ast.getChild(0).getValue() % ast.getChild(1).getValue()
+                else:
+                    print("invalid math operator found while constant folding")
 
                 ast.getParent().replaceChild(ast, AST.IntLit(value))
 
-        # TODO UNARY OPERATORS & LOGIC OPERATORS
+        elif isinstance(ast, AST.CompOp):
+            if isinstance(ast.getChild(0), AST.Literal) and isinstance(ast.getChild(1), AST.Literal):
+
+                value = False
+                if isinstance(ast, AST.Equal):
+                    value = ast.getChild(0).getValue() == ast.getChild(1).getValue()
+                elif isinstance(ast, AST.NotEqual):
+                    value = ast.getChild(0).getValue() != ast.getChild(1).getValue()
+                elif isinstance(ast, AST.More):
+                    value = ast.getChild(0).getValue() > ast.getChild(1).getValue()
+                elif isinstance(ast, AST.Less):
+                    value = ast.getChild(0).getValue() < ast.getChild(1).getValue()
+                elif isinstance(ast, AST.MoreE):
+                    value = ast.getChild(0).getValue() >= ast.getChild(1).getValue()
+                elif isinstance(ast, AST.LessE):
+                    value = ast.getChild(0).getValue() <= ast.getChild(1).getValue()
+                else:
+                    print("invalid comparison operator found while constant folding")
+
+                ast.getParent().replaceChild(ast, AST.Literal(value))
+
+        #TODO NOT Operator
+        elif isinstance(ast, AST.LogicOp):
+            if isinstance(ast.getChild(0), AST.Literal):
+                value = False
+
+                if isinstance(ast.getChild(1), AST.Literal):
+                    if isinstance(ast, AST.Or):
+                        value = ast.getChild(0).getValue() or ast.getChild(1).getValue()
+                    elif isinstance(ast, AST.And):
+                        value = not ast.getChild(0).getValue()
+                    else:
+                        print("invalid logic operator found while constant folding")
+
+                ast.getParent().replaceChild(ast, AST.IntLiteralLit(value))
+
+        # TODO Unary Operators
 
 
 
