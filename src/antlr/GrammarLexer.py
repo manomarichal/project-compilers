d55b37b754842caa1b2848794b00b5c6e947457c
sharogue <mano.marichal@student.uantwerpen.be>
2020-03-09 18:35:34 +0100

    floobits no plz

---
+++ src/antlr/GrammarLexer.py
@@ -0,0 +1,143 @@
+# Generated from src/Grammar.g4 by ANTLR 4.8
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2 ")
+        buf.write("\u00a3\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+        buf.write("\4\31\t\31\4\32\t\32\4\33\t\33\4\34\t\34\4\35\t\35\4\36")
+        buf.write("\t\36\4\37\t\37\3\2\3\2\3\2\3\2\3\3\6\3E\n\3\r\3\16\3")
+        buf.write("F\3\4\6\4J\n\4\r\4\16\4K\3\4\3\4\6\4P\n\4\r\4\16\4Q\3")
+        buf.write("\5\3\5\3\5\3\5\3\6\3\6\3\6\3\6\3\6\3\6\3\7\3\7\3\7\3\7")
+        buf.write("\3\7\3\b\3\b\3\b\3\b\3\b\3\b\3\t\3\t\3\n\3\n\3\13\3\13")
+        buf.write("\3\f\3\f\3\r\3\r\3\16\3\16\3\17\3\17\3\20\3\20\3\21\3")
+        buf.write("\21\3\22\3\22\3\23\3\23\3\24\3\24\3\25\3\25\3\25\3\26")
+        buf.write("\3\26\3\26\3\27\3\27\3\27\3\30\3\30\3\30\3\31\3\31\3\31")
+        buf.write("\3\32\3\32\3\32\3\33\3\33\3\33\3\34\3\34\3\34\3\35\3\35")
+        buf.write("\3\36\3\36\3\37\6\37\u009e\n\37\r\37\16\37\u009f\3\37")
+        buf.write("\3\37\2\2 \3\3\5\4\7\5\t\6\13\7\r\b\17\t\21\n\23\13\25")
+        buf.write("\f\27\r\31\16\33\17\35\20\37\21!\22#\23%\24\'\25)\26+")
+        buf.write("\27-\30/\31\61\32\63\33\65\34\67\359\36;\37= \3\2\6\3")
+        buf.write("\2\"\u0080\3\2\62;\3\2c|\5\2\13\f\17\17\"\"\2\u00a6\2")
+        buf.write("\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\2\13\3")
+        buf.write("\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3\2")
+        buf.write("\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2\2")
+        buf.write("\2\2\35\3\2\2\2\2\37\3\2\2\2\2!\3\2\2\2\2#\3\2\2\2\2%")
+        buf.write("\3\2\2\2\2\'\3\2\2\2\2)\3\2\2\2\2+\3\2\2\2\2-\3\2\2\2")
+        buf.write("\2/\3\2\2\2\2\61\3\2\2\2\2\63\3\2\2\2\2\65\3\2\2\2\2\67")
+        buf.write("\3\2\2\2\29\3\2\2\2\2;\3\2\2\2\2=\3\2\2\2\3?\3\2\2\2\5")
+        buf.write("D\3\2\2\2\7I\3\2\2\2\tS\3\2\2\2\13W\3\2\2\2\r]\3\2\2\2")
+        buf.write("\17b\3\2\2\2\21h\3\2\2\2\23j\3\2\2\2\25l\3\2\2\2\27n\3")
+        buf.write("\2\2\2\31p\3\2\2\2\33r\3\2\2\2\35t\3\2\2\2\37v\3\2\2\2")
+        buf.write("!x\3\2\2\2#z\3\2\2\2%|\3\2\2\2\'~\3\2\2\2)\u0080\3\2\2")
+        buf.write("\2+\u0083\3\2\2\2-\u0086\3\2\2\2/\u0089\3\2\2\2\61\u008c")
+        buf.write("\3\2\2\2\63\u008f\3\2\2\2\65\u0092\3\2\2\2\67\u0095\3")
+        buf.write("\2\2\29\u0098\3\2\2\2;\u009a\3\2\2\2=\u009d\3\2\2\2?@")
+        buf.write("\7)\2\2@A\t\2\2\2AB\7)\2\2B\4\3\2\2\2CE\t\3\2\2DC\3\2")
+        buf.write("\2\2EF\3\2\2\2FD\3\2\2\2FG\3\2\2\2G\6\3\2\2\2HJ\t\3\2")
+        buf.write("\2IH\3\2\2\2JK\3\2\2\2KI\3\2\2\2KL\3\2\2\2LM\3\2\2\2M")
+        buf.write("O\7\60\2\2NP\t\3\2\2ON\3\2\2\2PQ\3\2\2\2QO\3\2\2\2QR\3")
+        buf.write("\2\2\2R\b\3\2\2\2ST\7k\2\2TU\7p\2\2UV\7v\2\2V\n\3\2\2")
+        buf.write("\2WX\7h\2\2XY\7n\2\2YZ\7q\2\2Z[\7c\2\2[\\\7v\2\2\\\f\3")
+        buf.write("\2\2\2]^\7e\2\2^_\7j\2\2_`\7c\2\2`a\7t\2\2a\16\3\2\2\2")
+        buf.write("bc\7e\2\2cd\7q\2\2de\7p\2\2ef\7u\2\2fg\7v\2\2g\20\3\2")
+        buf.write("\2\2hi\7?\2\2i\22\3\2\2\2jk\7-\2\2k\24\3\2\2\2lm\7(\2")
+        buf.write("\2m\26\3\2\2\2no\7/\2\2o\30\3\2\2\2pq\7,\2\2q\32\3\2\2")
+        buf.write("\2rs\7\61\2\2s\34\3\2\2\2tu\7\'\2\2u\36\3\2\2\2vw\7*\2")
+        buf.write("\2w \3\2\2\2xy\7+\2\2y\"\3\2\2\2z{\7=\2\2{$\3\2\2\2|}")
+        buf.write("\7>\2\2}&\3\2\2\2~\177\7@\2\2\177(\3\2\2\2\u0080\u0081")
+        buf.write("\7?\2\2\u0081\u0082\7?\2\2\u0082*\3\2\2\2\u0083\u0084")
+        buf.write("\7>\2\2\u0084\u0085\7?\2\2\u0085,\3\2\2\2\u0086\u0087")
+        buf.write("\7@\2\2\u0087\u0088\7?\2\2\u0088.\3\2\2\2\u0089\u008a")
+        buf.write("\7#\2\2\u008a\u008b\7?\2\2\u008b\60\3\2\2\2\u008c\u008d")
+        buf.write("\7(\2\2\u008d\u008e\7(\2\2\u008e\62\3\2\2\2\u008f\u0090")
+        buf.write("\7~\2\2\u0090\u0091\7~\2\2\u0091\64\3\2\2\2\u0092\u0093")
+        buf.write("\7-\2\2\u0093\u0094\7-\2\2\u0094\66\3\2\2\2\u0095\u0096")
+        buf.write("\7/\2\2\u0096\u0097\7/\2\2\u00978\3\2\2\2\u0098\u0099")
+        buf.write("\7#\2\2\u0099:\3\2\2\2\u009a\u009b\t\4\2\2\u009b<\3\2")
+        buf.write("\2\2\u009c\u009e\t\5\2\2\u009d\u009c\3\2\2\2\u009e\u009f")
+        buf.write("\3\2\2\2\u009f\u009d\3\2\2\2\u009f\u00a0\3\2\2\2\u00a0")
+        buf.write("\u00a1\3\2\2\2\u00a1\u00a2\b\37\2\2\u00a2>\3\2\2\2\7\2")
+        buf.write("FKQ\u009f\3\b\2\2")
+        return buf.getvalue()
+
+
+class GrammarLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    CHAR = 1
+    INT = 2
+    FLOAT = 3
+    INT_TYPE = 4
+    FLOAT_TYPE = 5
+    CHAR_TYPE = 6
+    CONST = 7
+    ASSIGN_OP = 8
+    PLUS = 9
+    AMP = 10
+    MINUS = 11
+    STAR = 12
+    SLASH = 13
+    PERCENT = 14
+    LEFT_PAREN = 15
+    RIGHT_PAREN = 16
+    SEMICOLON = 17
+    SMALLER_OP = 18
+    GREATER_OP = 19
+    EQUAL_OP = 20
+    SMALLER_E_OP = 21
+    GREATER_E_OP = 22
+    NOT_EQUAL_OP = 23
+    AND_OP = 24
+    OR_OP = 25
+    INCR = 26
+    DECR = 27
+    NOT_OP = 28
+    ID = 29
+    WS = 30
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'int'", "'float'", "'char'", "'const'", "'='", "'+'", "'&'", 
+            "'-'", "'*'", "'/'", "'%'", "'('", "')'", "';'", "'<'", "'>'", 
+            "'=='", "'<='", "'>='", "'!='", "'&&'", "'||'", "'++'", "'--'", 
+            "'!'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "CHAR", "INT", "FLOAT", "INT_TYPE", "FLOAT_TYPE", "CHAR_TYPE", 
+            "CONST", "ASSIGN_OP", "PLUS", "AMP", "MINUS", "STAR", "SLASH", 
+            "PERCENT", "LEFT_PAREN", "RIGHT_PAREN", "SEMICOLON", "SMALLER_OP", 
+            "GREATER_OP", "EQUAL_OP", "SMALLER_E_OP", "GREATER_E_OP", "NOT_EQUAL_OP", 
+            "AND_OP", "OR_OP", "INCR", "DECR", "NOT_OP", "ID", "WS" ]
+
+    ruleNames = [ "CHAR", "INT", "FLOAT", "INT_TYPE", "FLOAT_TYPE", "CHAR_TYPE", 
+                  "CONST", "ASSIGN_OP", "PLUS", "AMP", "MINUS", "STAR", 
+                  "SLASH", "PERCENT", "LEFT_PAREN", "RIGHT_PAREN", "SEMICOLON", 
+                  "SMALLER_OP", "GREATER_OP", "EQUAL_OP", "SMALLER_E_OP", 
+                  "GREATER_E_OP", "NOT_EQUAL_OP", "AND_OP", "OR_OP", "INCR", 
+                  "DECR", "NOT_OP", "ID", "WS" ]
+
+    grammarFileName = "Grammar.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.8")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
---
+++ src/antlr/GrammarVisitor.py
@@ -0,0 +1,38 @@
+# Generated from src/Grammar.g4 by ANTLR 4.8
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .GrammarParser import GrammarParser
+else:
+    from GrammarParser import GrammarParser
+
+# This class defines a complete generic visitor for a parse tree produced by GrammarParser.
+
+class GrammarVisitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by GrammarParser#doc.
+    def visitDoc(self, ctx:GrammarParser.DocContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#typeObj.
+    def visitTypeObj(self, ctx:GrammarParser.TypeObjContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#decl.
+    def visitDecl(self, ctx:GrammarParser.DeclContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#literal.
+    def visitLiteral(self, ctx:GrammarParser.LiteralContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#expr.
+    def visitExpr(self, ctx:GrammarParser.ExprContext):
+        return self.visitChildren(ctx)
+
+
+
+del GrammarParser
