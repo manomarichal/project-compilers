d6071086ad00d59e3901a36affb6406bc340dbae
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2020-03-16 16:36:19 +0100

    first iteration of TypeAssignVisitor + observer (safety commit, may be broken)

--- src/AST/TypeAssignVisitor.py
+++ src/AST/TypeAssignVisitor.py
@@ -4,48 +4,81 @@ from src.utility.TypeClass import TypeClass, TypeComponents
 from copy import deepcopy
 
 
+class TypeListener:
+    def update(self, node, node_type, child_types):
+        try_type = type(node)
+        while not hasattr(self, "update"+try_type.__name__):
+            if try_type == Component:
+                break
+            try_type = type(super(try_type, node))
+        else:
+            return
+        getattr(self, "update"+try_type.__name__)(node, node_type, child_types)
+
+
 class TypeAssignVisitor (Visitor):
     def __init__(self):
         self.warnings = []
+        self.observers = []
+
+    def attach(self, listener: TypeListener):
+        self.observers.append(listener)
+
+    def detach(self, listener: TypeListener):
+        self.observers.remove(listener)
+
+    def notify(self, node, node_type, child_types):
+        for observer in self.observers:
+            observer.update(node, node_type, child_types)
 
     def implicit_conversion_warning(self, from_type: TypeClass, to_type: TypeClass):
         self.warnings.append("implicitly converting " + str(from_type) + " to " + str(to_type))
 
     def unary_propagate(self, node):
-        return self.visit(node.get_child(0))
+        child_type = self.visit(node.get_child(0))
+        self.notify(node, child_type, [child_type])
+        return child_type
 
     def visit(self, node) -> TypeClass:
         return Visitor.visit(self, node)
 
     def visitLeaf(self, node: Leaf):
-        return node.get_type()
+        own_type = node.get_type()
+        self.notify(node, [], own_type)
+        return own_type
 
     def visitMathop(self, node: MathOp):
         a_type = self.visit(node.get_child(0))
         b_type = self.visit(node.get_child(1))
+        own_type = None
         if a_type == b_type:
-            return a_type
-        if b_type.promotes_to(a_type):
-            return a_type
-        if a_type.promotes_to(b_type):
-            return b_type
-        if b_type.converts_to(a_type) and not a_type.converts_to(b_type):
+            own_type = a_type
+        elif b_type.promotes_to(a_type):
+            own_type = a_type
+        elif a_type.promotes_to(b_type):
+            own_type = b_type
+        elif b_type.converts_to(a_type) and not a_type.converts_to(b_type):
             self.implicit_conversion_warning(b_type, a_type)
-            return a_type
-        if a_type.converts_to(b_type) and not b_type.converts_to(a_type):
+            own_type = a_type
+        elif a_type.converts_to(b_type) and not b_type.converts_to(a_type):
             self.implicit_conversion_warning(b_type, a_type)
-            return b_type
+            own_type = b_type
+        self.notify(node, own_type, [a_type, b_type])
+        return own_type
 
     def visitAssignOp(self, node):
         a_type = self.visit(node.get_child(0))
         b_type = self.visit(node.get_child(1))
+        own_type = None
         if a_type == b_type:
-            return a_type
-        if b_type.promotes_to(a_type):
-            return a_type
-        if b_type.converts_to(a_type) and not a_type.converts_to(b_type):
+            own_type = a_type
+        elif b_type.promotes_to(a_type):
+            own_type = a_type
+        elif b_type.converts_to(a_type) and not a_type.converts_to(b_type):
             self.implicit_conversion_warning(b_type, a_type)
-            return a_type
+            own_type = a_type
+        self.notify(node, own_type, [a_type, b_type])
+        return own_type
 
     def visitPos(self, node):
         return self.unary_propagate(node)
@@ -78,6 +111,8 @@ class TypeAssignVisitor (Visitor):
                 all_convert = False
             warnings.append(child_type)
 
+        self.notify(node, bool_type, child_types)
+
         if all_promote:
             return bool_type
         if all_convert:
@@ -89,6 +124,9 @@ class TypeAssignVisitor (Visitor):
         a_type = self.visit(node.get_child(0))
         b_type = self.visit(node.get_child(0))
         bool_type = TypeClass([TypeComponents.BOOL])
+
+        self.notify(node, bool_type, [a_type, b_type])
+
         if a_type == b_type:
             return bool_type
         if b_type.promotes_to(a_type):
@@ -103,10 +141,16 @@ class TypeAssignVisitor (Visitor):
             return bool_type
 
     def visitAdress(self, node: Adress):
-        return deepcopy(self.visit(node.get_child(0))).pushType(TypeComponents.PTR)
+        child_type = self.visit(node.get_child(0))
+        own_type = deepcopy(child_type).pushType(TypeComponents.PTR)
+        self.notify(node, own_type, [child_type])
+        return own_type
 
     def visitIndir(self, node: Indir):
-        child_type = deepcopy(self.visit(node.get_child(0)))
+        child_type = self.visit(node.get_child(0))
+        own_type = None
         if child_type.is_ptr():
-            child_type.popType()
-            return child_type
+            own_type = deepcopy(child_type)
+            own_type.popType()
+        self.notify(node, own_type, [child_type])
+        return own_type
