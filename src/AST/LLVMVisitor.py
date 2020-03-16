4cdea29969f5c0329616888c0d78c53aa4fcbe33
sharogue <mano.marichal@student.uantwerpen.be>
2020-03-16 20:09:06 +0100

    formatting

--- src/AST/LLVMVisitor.py
+++ src/AST/LLVMVisitor.py
@@ -1,25 +1,57 @@
 from src.AST import AST
 from src.AST.Visitor import Visitor
+from enum import Enum
+from termcolor import colored, cprint
+
+
+def to_llvm_type(var) -> str:
+    var_type = var.get_type().__repr__()
+    if var_type == 'int':
+        return 'i32'
+    elif var_type == 'float':
+        return 'float'
+    elif var_type == 'char':
+        return 'i32'
+    elif var_type == 'None':
+        # TODO literals hebben soms none type
+        return 'none'
+    else:
+        print('invalid type')
+        exit(0)
+
+
+def math_opp(res, lhs: AST.Component ,rhs: AST.Component, ast: AST.MathOp):
+
+    if isinstance(ast, AST.Sum):
+        op_str = ' add '
+        op_com = ' + '
+    elif isinstance(ast, AST.Sub):
+        op_str = ' sub '
+        op_com = ' - '
+    elif isinstance(ast, AST.Prod):
+        op_str = ' mul '
+        op_com = ' * '
+    elif isinstance(ast, AST.Div):
+        op_str = ' mod '
+        op_com = ' / '
+    elif isinstance(ast, AST.Mod):
+        op_str = ' mod '
+        op_com = ' % '
+    else:
+        print("invalid math operator found while constant folding")
+
+    lhs_reg = lhs.get_register()
+    rhs_reg = lhs.get_register()
+
+    comment = colored(' // ' + res + ' = ' + lhs_reg + op_com + rhs_reg + '\t', 'green')
+    print("{: <35} ".format(res + ' =' + op_str + to_llvm_type(lhs) + ', *' + lhs_reg))
+    print("{: <35} {: <35} ".format(res + ' =' + op_str + to_llvm_type(rhs) + ', *' + rhs_reg, comment))
 
 class LLVMVisitor(Visitor):
 
     _rcounter = 0
 
-    def _to_llvm_type(ast, var) -> str:
-        var_type = var.get_type().__repr__()
-        if var_type == 'int':
-            return 'i32'
-        elif var_type == 'float':
-            return 'float'
-        elif var_type == 'char':
-            return 'i32'
-        elif var_type == 'None':
-            #TODO literals hebben soms none type
-            return 'none'
-        else:
-            print('invalid type')
-            exit(0)
-
+    # HELPER FUNCTIONS
     def _get_rname(self) -> str:
         self._rcounter += 1
         return '%t' + str(self._rcounter)
@@ -27,12 +59,12 @@ class LLVMVisitor(Visitor):
     def visitComposite(self, ast: AST.Composite):
         self.visitChildren(ast)
 
+
     def visitDecl(self, ast: AST.Decl):
         var: AST.Variable = ast.get_child(0)
-        comment = 'allocate ' + var.get_name() + ' in ' + var.get_register()
-
-        print('\n// ' + comment)
-        print('%' + var.get_name() + ' = alloca ' + self._to_llvm_type(var))
+        comment = colored('// init '+ var.get_register() + ' as ' + var.get_name() + '\t', 'green')
+        
+        print("{: <35} {: <35} ".format('%' + var.get_name() + ' = alloca ' + to_llvm_type(var), comment))
 
     def visitAssignOp(self, ast:AST.AssignOp):
         self.visitChildren(ast)
@@ -44,20 +76,28 @@ class LLVMVisitor(Visitor):
             var: AST.Variable = ast.get_child(0)
 
         reg = '%' + var.get_name()
+        
+        comment = colored(' // assign ' + ast.get_child(1).get_register() + ' to ' + reg + '\t', 'green')
+        print("{: <35} {: <35} ".format(reg + ' = load ' + to_llvm_type(var) + ', *' + str(ast.get_child(1).get_register()), comment))
 
-        print('\n// assign ' + ast.get_child(1).get_register() + ' to ' + reg)
-        print(reg + ' = load ' + self._to_llvm_type(var) + '*, ' + str(ast.get_child(1).get_register()))
+    def visitMathOp(self, ast: AST.MathOp):
 
-    def visitSum(self, ast: AST.Sum):
         self.visitChildren(ast)
-        ast.set_register(self._get_rname())
+
+        if not (isinstance(ast.get_child(0), AST.Literal) and isinstance(ast.get_child(1), AST.Literal)): return
+
+
+        self.visitChildren(ast)
+        reg = self._get_rname()
+        ast.set_register(reg)
+        math_opp(reg, ast.get_child(0), ast.get_child(1), )
 
     def visitLiteral(self, ast: AST.Literal):
         reg = self._get_rname()
-        print('\n// load ' + str(ast.get_value()) + ' in ' + reg)
-        print(reg + ' = load ' + self._to_llvm_type(ast) + ', ' + str(ast.get_value()))
         ast.set_register(reg)
 
+        comment = colored(' // load ' + str(ast.get_value()) + ' in ' + reg + '\t', 'green')
+        print("{: <35} {: <35} ".format(reg + ' = load ' + to_llvm_type(ast) + ', ' + str(ast.get_value()), comment))
 
     
 
