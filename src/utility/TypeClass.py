0927a3f73d6e29c224fd7bec5c63410871e48e43
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2020-03-16 11:06:06 +0100

    switched to enum types

--- src/utility/TypeClass.py
+++ src/utility/TypeClass.py
@@ -1,9 +1,23 @@
 import copy
 
-PrimitiveComp = {"char", "int", "float"}
-ModifierComp = {"const", "*"}
-TypeComp = copy.deepcopy(PrimitiveComp)
-TypeComp.union(ModifierComp)
+
+class TypeComponents:
+    BOOL = 1
+    CHAR = 2
+    INT = 3
+    FLOAT = 4
+    CONST = 5
+    PTR = 6
+
+    primitive = {BOOL, CHAR, INT, FLOAT}
+    modifier = {CONST, PTR}
+
+    translations = {BOOL: "bool",
+                    CHAR: "char",
+                    INT: "int",
+                    FLOAT: "float",
+                    CONST: "const",
+                    PTR: "*"}
 
 
 class TypeClass:
@@ -11,11 +25,14 @@ class TypeClass:
     # [1:] = type modifier
     _type_stack = []
 
-    def __init__(self, type):
-        self._type_stack = type
+    def __init__(self, type_stack):
+        self._type_stack = type_stack
 
-    def pushType(self, newType):
-        self._type_stack.append(newType)
+    def pushType(self, new_type):
+        self._type_stack.append(new_type)
+
+    def popType(self):
+        self._type_stack.pop()
 
     def __eq__(self, other):
         return other.getType() == self.getType()
@@ -27,18 +44,18 @@ class TypeClass:
         return self._type_stack
 
     def is_const(self) -> bool:
-        tmp = self.get_top_type() == "const"
+        tmp = self.get_top_type() == TypeComponents.CONST
         return tmp
 
     def is_ptr(self) -> bool:
-        return self.get_top_type("const") == "*"
+        return self.get_top_type(TypeComponents.CONST) == TypeComponents.PTR
 
     def promotes_to(self, other):
-        self_type = self.get_top_type("const")
-        other_type = other.get_top_type("const")
-        if self_type == "char" and other_type in {"int", "float"}:
+        self_type = self.get_top_type(TypeComponents.CONST)
+        other_type = other.get_top_type(TypeComponents.CONST)
+        if self_type == TypeComponents.CHAR and other_type in {TypeComponents.INT, TypeComponents.FLOAT}:
             return True
-        if self_type == "int" and other_type == "float":
+        if self_type == TypeComponents.INT and other_type == TypeComponents.FLOAT:
             return True
         return False
 
@@ -57,5 +74,5 @@ class TypeClass:
     def __repr__(self):
         result = ""
         for component in self.getType():
-            result += component + " "
+            result += TypeComponents.translations[component] + " "
         return result[0:len(result)-1]
