3815b841a1d8849b1e625f6252aaf4291868a3ec
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2020-03-17 16:43:58 +0100

    functional type assignment to AST

--- src/main.py
+++ src/main.py
@@ -1,35 +1,58 @@
+# external libraries
 import sys
 import copy
 from antlr4 import *
 
-from src.AST.CFVisitor import CFVisitor
+# generated ANTLR code
 from src.antlr.GrammarLexer import GrammarLexer
 from src.antlr.GrammarParser import GrammarParser
+
+# generic compiler code
+from src.AST.Visitor import Visitor as ASTVisitor
+from src.AST.AST import Component
+from src.utility.SemanticExceptions import SemanticError, SemanticWarning
+
+# AST passes
+from src.AST.CFVisitor import CFVisitor
 from src.CST.Visitor import Visitor as CSTVisitor
 from src.AST.DotVisitor import DotVisitor
-from src.AST.SemanticVisitor import SemanticVisitor
 from src.AST.LLVMVisitor import LLVMVisitor
+from src.AST.TypeVisitor import TypeVisitor
+
+# config through import & alias
+from src.AST.DotVisitor import label_small as label_style
+
 
 # Mandatory
 # P2
-#TODO semantic erros -> zo (J)
-#TODO syntax errors -> zo (J)
-#TODO pointer operators & assigment (LVAL vs RVAL) -> zo (M)
-#TODO pointer grammar -> ma (M)
-#TODO pointer semantic errors -> deadline
-#TODO constant propagation until next assignment -> zo
+# TODO semantic erros -> zo (J)
+# TODO syntax errors -> zo (J)
+# TODO pointer operators & assigment (LVAL vs RVAL) -> zo (M)
+# TODO pointer grammar -> ma (M)
+# TODO pointer semantic errors -> deadline
+# TODO constant propagation until next assignment -> zo
 
 # P3
-#TODO printf() -> ma
-#TODO to LLVM -> ma
-#TODO scripts + readme + verslag -> deadline
+# TODO printf() -> ma
+# TODO to LLVM -> ma
+# TODO scripts + readme + verslag -> deadline
 
 # Optional
+# TODO ++--
+# TODO implicit not python conversions
+# TODO dont ignore comments
+# TODO comment after every instruction
 
-#TODO ++--
-#TODO implicit not python conversions
-#TODO dont ignore comments
-#TODO comment after every instruction
+
+def ast_pass(visitor: ASTVisitor, tree: Component):
+    try:
+        return visitor, visitor.visit(tree)
+    except SemanticError as oops:
+        print(oops, sys.stderr)
+        exit(1)
+    except SemanticWarning as oops:
+        print(oops, sys.stderr)
+
 
 
 def main(argv):
@@ -42,19 +65,21 @@ def main(argv):
     visitor = CSTVisitor()
     ast = visitor.visit(tree)
 
-    semantic_checker = SemanticVisitor()
-    semantic_checker.visit(ast)
+    # semantic_checker = SemanticVisitor()
+    # semantic_checker.visit(ast)
 
-    # constant_folding = CFVisitor()
-    # constant_folding.visit(ast)
+    ast_pass(TypeVisitor(), ast)
 
-    llvm = LLVMVisitor()
-    llvm.visit(ast)
+    ast_pass(CFVisitor(), ast)
 
-    astVisualiser = DotVisitor()
-    astVisualiser.visit(ast)
-    astVisualiser.graph.write("test_IO/result.dot")
-    astVisualiser.graph.write_png("test_IO/result.png")
+    tfile = open('./test_IO/result.ll', 'w+')
+    ast_pass(LLVMVisitor(tfile), ast)
+    tfile.write('\n}')
+    tfile.close()
+
+    visualisation: DotVisitor = ast_pass(DotVisitor(label_style), ast)[0]
+    visualisation.graph.write("test_IO/result.dot")
+    visualisation.graph.write_png("test_IO/result.png")
 
 
 if __name__ == '__main__':
