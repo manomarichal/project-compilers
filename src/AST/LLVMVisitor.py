3b2d7767afad4df1eebac3a3e188f8d4046d2381
sharogue <mano.marichal@student.uantwerpen.be>
2020-03-19 15:39:34 +0100

    refactor

--- src/AST/LLVMVisitor.py
+++ src/AST/LLVMVisitor.py
@@ -22,7 +22,7 @@ def get_math_instruction(op:AST.MathOp, floating: bool):
         op_str += 'mul '
         op_com = ' * '
     elif isinstance(op, AST.Div):
-        if (op_str == ' f'): op_str = ' fdiv '
+        if op_str == ' f': op_str = ' fdiv '
         else: op_str += 'sdiv '
         op_com = ' / '
     elif isinstance(op, AST.Mod):
@@ -30,15 +30,6 @@ def get_math_instruction(op:AST.MathOp, floating: bool):
         op_com = ' % '
 
     return op_str, op_com
-
-def check_var_type(var: AST.Variable):
-    value = var.get_val()
-    if isinstance(value, int):
-        var.set_type(TypeClass([3]))
-    if isinstance(value, float):
-        var.set_type(TypeClass([4]))
-    else:
-        var.set_type(TypeClass([3]))
 
 def to_llvm_type(var) -> str:
     var_type = var.get_type().__repr__()
@@ -48,13 +39,9 @@ def to_llvm_type(var) -> str:
         return 'float'
     elif var_type == 'char':
         return 'i8'
-    elif var_type == 'None':
-        # TODO literals hebben soms none type
-        return 'none'
     else:
         print('invalid type')
         exit(0)
-
 
 class LLVMVisitor(Visitor):
     file = None
@@ -67,11 +54,6 @@ class LLVMVisitor(Visitor):
 
     # HELPER FUNCTIONS
     def print_to_file(self, string, comment=None):
-        # if comment is not None:
-        #     color = colored(comment, 'green')
-        #     print("{: <30} {: <30} ".format(string, color))
-        # else:
-        #     print("{: <30}".format(string))
         self.file.write('\n\t; ' + comment)
         self.file.write('\n\t\t' + string)
 
@@ -84,6 +66,7 @@ class LLVMVisitor(Visitor):
         return '%t' + str(self._rcounter)
     
     def get_reg(self, ast: AST.Component):
+        # variables have to be loaded in before being used
         if isinstance(ast, AST.Variable):
             return self.generate_load(ast)
         else:
@@ -103,7 +86,6 @@ class LLVMVisitor(Visitor):
         self.print_to_file(string, comment)
 
     def generate_math_instr(self, res, lhs, rhs, type_of, op: AST.MathOp):
-
         op_str, op_com = get_math_instruction(op, type_of == 'float')
         comment = res + ' = ' + str(lhs) + op_com + str(rhs)
         string = res + ' =' + op_str + type_of + ' ' + str(lhs) + ', ' + str(rhs)
@@ -149,17 +131,14 @@ class LLVMVisitor(Visitor):
             c = '0.0'
         else:
             c = '0'
-
         self.generate_math_instr(reg, ast.get_value(), c, to_llvm_type(ast), AST.Sum())
 
-    def visitPrintf(self, ast: AST.Printf):  # TODO
+    def visitPrintf(self, ast: AST.Printf):  #
         self.visitChildren(ast)
-
         if isinstance(ast.get_child(0), AST.Variable):
             reg = self.generate_load(ast.get_child(0))
         else:
             reg = ast.get_child(0).get_register()
-
         self.generate_printf(reg, to_llvm_type(ast.get_child(0)))
 
     def visitIncrPre(self, ast: AST.IncrPre):
