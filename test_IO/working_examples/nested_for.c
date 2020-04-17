f428f132f399d5547ad197a6b8ebf1cf2b19145b
sharogue <mano.marichal@student.uantwerpen.be>
2020-04-17 18:43:28 +0200

    continue & break

--- test_IO/working_examples/nested_for.c
+++ test_IO/working_examples/nested_for.c
@@ -1,4 +1,4 @@
-int nested_for() // returns 6*8
+int nested_for1()
 {
     int t = 0;
     for (int b=0;b<10;b = b +1)
@@ -27,11 +27,39 @@ int nested_for() // returns 6*8
     return t;
 }
 
-
+int nested_for2()
+{
+    int t = 0;
+    for (int b=0;b<10;b = b +1)
+    {
+        if (b < 3)
+        {
+            continue;
+        }
+        for (int c=0;c<10;c = c +1)
+        {
+            if (b < 4)
+            {
+                continue;
+            }
+            t = t + 1;
+            if (c == 6)
+            {
+                break;
+            }
+        }
+        if (b == 7)
+        {
+            break;
+        }
+    }
+    return t;
+}
 int main()
 {
-    int res = nested_for();
-    printf(res);    // expected 72
+    int res = nested_for1();
+    int res2 = nested_for2();
+    int total = res + res2;
+    printf(total);    // expected 76
     return 0;
 }
-
