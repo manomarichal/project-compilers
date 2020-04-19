d13938b6f4300ead6d5a3ed3c893a5464fe6b075
Joren Van Borm <Joren.VanBorm@student.uantwerpen.be>
2020-04-19 23:21:01 +0200

    re-added semantic error stuffs

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ print("\n"
       "Error detection tests below this line - supposed to fail!\n"
       "---------------------------------------------------------\n")
 
-# os.system('find ./test_IO/CompilersBenchmark/SemanticErrors -iname \'*.c\' -exec echo compiling {} \; -exec python ./src/main.py {} \;')
+os.system('find ./test_IO/CompilersBenchmark/SemanticErrors -iname \'*.c\' -exec echo compiling {} \; -exec python ./src/main.py {} \;')
