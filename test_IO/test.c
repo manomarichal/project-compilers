79cac2a23f4dea92fbb3321aff30bee067790951
sharogue <mano.marichal@student.uantwerpen.be>
2020-05-21 15:28:54 +0200

    float mul & div

--- test_IO/test.c
+++ test_IO/test.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    int a = 10;
-    float c = 4.2;
-    float k = 4.2 + 5.8 + 1.8;
-    printf("%d", k);
-    printf("%d", a);
+    float f = 10.0/3.0;
+    printf("%d", f);
 }
