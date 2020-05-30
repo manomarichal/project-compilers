c1bf853acc097748dd17bc48b72dc006421d09fc
sharogue <mano.marichal@student.uantwerpen.be>
2020-05-30 12:01:27 +0200

    scriptje nog cooler maken

--- run_test_files.py
+++ run_test_files.py
@@ -4,16 +4,19 @@ os.system('python3 clean.py')
 files = os.listdir('./test_IO/CompilersBenchmark/CorrectCode')
 print(os.path.abspath("./"))
 for file in files:
-    input("")
+    if file == '.DS_Store': continue
     os.system("clear")
     file_path = "./test_IO/CompilersBenchmark/CorrectCode/" + file
-    print("\n##########################\nfile: " + file)
+    print("##########################\nfile: " + file)
     os.system("cat " + file_path)
-    print("compiling " + file)
-    os.system("python3 ./src/main.py " + file_path + " -n")
+    print("\n##########################\ncompiling " + file)
+    os.system("python3 ./src/main.py " + file_path)
     asm_file_path =  file_path[0:len(file_path)-2] + ".asm"
-    print("running " + asm_file_path)
+    print("\n##########################\nrunning " + asm_file_path)
     os.system("java -jar Mars4_5.jar " + asm_file_path)
+    print("##########################")
+    input("")
+
 
 
 
